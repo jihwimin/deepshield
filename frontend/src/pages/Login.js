669b70bc94392c8fb2b8dec3126365c6de39a042
jihwimin <jihwimin@gmail.com>
2025-01-25 15:48:00 -0500

    saves nickname and token in context for authentication, Redirects to /dashboard after login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import API_BASE_URL from "../config";
+import AuthContext from "../context/AuthContext";
 
 const Login = () => {
+  const { login } = useContext(AuthContext);
   const [formData, setFormData] = useState({ username: "", password: "" });
   const navigate = useNavigate();
 
@@ -13,11 +15,11 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(`${API_BASE_URL}/api/auth/login`, formData);
-      localStorage.setItem("token", res.data.token);
+      login(res.data.nickname, res.data.token);
       alert("Login successful!");
-      navigate("/");  // Redirect after login
+      navigate("/dashboard"); // Redirect after login
     } catch (err) {
-      alert("Invalid credentials");
+      alert(err.response?.data?.error || "Login failed. Please try again.");
     }
   };
 
