ed6598dcefde92f9168713d2176a6a39c8717ce9
jihwimin <jihwimin@gmail.com>
2025-01-25 14:02:37 -0500

    restart backend

--- backend/deepshield_project/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-# accounts/urls.py
-from django.urls import path
-from .views import register_view, CustomLoginView, CustomLogoutView
-
-urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-]
