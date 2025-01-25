793e5a9687c2c5cbe2d7c9c06a95a1d6a9c198b6
jihwimin <jihwimin@gmail.com>
2025-01-25 15:48:54 -0500

    wrap everything inside AuthProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+};
+
+const ProtectedRoute = ({ component: Component }) => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  return storedUser ? <Component /> : <Navigate to="/login" />;
+};
 
 export default App;
